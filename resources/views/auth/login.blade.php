32c610f212eb6b55a65028c98844a1a0430079aa
MFRHoldings <rick@mfrholdings.com>
2022-01-17 12:32:21 -0500

    Standardize login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="text-white">
         <img
             src="https://njacda.com/wp-content/uploads/2014/01/1524725_10206920933608488_2344636879262137296_n-960x430.jpg"
-            width="60%" style="margin: auto;">
+            height="50%" style="margin: auto;">
     </div>
     <section class="relative w-full h-full py-40 min-h-screen">
         <div class="container mx-auto px-4 h-full">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+    <x-menus.guest/>
+    <div class="text-white">
+        <img
+            src="https://njacda.com/wp-content/uploads/2014/01/1524725_10206920933608488_2344636879262137296_n-960x430.jpg"
+            height="50%"  style="margin: auto;">
+    </div>
 <section class="relative w-full h-full py-40 min-h-screen">
     <div class="container mx-auto px-4 h-full">
         <div class="flex content-center items-center justify-center h-full">

f0cdc468f0f4d09fd689dd1607bd3ff0e60d45c3
MFRHoldings <rick@mfrholdings.com>
2022-02-15 17:16:52 -0500

    Create migration.

---
+++ database/migrations/2022_02_15_221411_create_venues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVenuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venues', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained();
+            $table->string('name',60);
+            $table->string('shortname',16);
+            $table->string('building',120)->nullable();
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venues');
+    }
+}

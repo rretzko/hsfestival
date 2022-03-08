2317283a2584fc7cabac83d7cdea749cb37e1fae
MFRHoldings <rick@mfrholdings.com>
2022-03-08 10:44:17 -0500

    Restrict access to /admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Ensemble;
+use App\Models\Useroption;
+
 class HomeController
 {
     public function index()
     {
-        return view('admin.home');
+        if((auth()->id() === 1) || (auth()->id() ===27))
+        {
+            return view('admin.home');
+        }else{
+
+            return view('users.home',
+                [
+                    'ensembles' => Ensemble::where('user_id', auth()->id())
+                        ->where('school_id', auth()->user()->school->id)
+                        ->get(),
+                    'useroptions' => Useroption::where('user_id', auth()->id())->get(),
+                ]
+            );
+        }
     }
 }
